1f2feaed3af316ff746805ca10d9b3abfb5734b1
Cypher Riot <CypherRiot@protonmail.com>
2025-07-10 16:51:10 -0700

    Removed Ruby and added hyprsunset (blue light reduction)

--- install/desktop.sh
+++ install/desktop.sh
@@ -3,6 +3,7 @@ yay -S --noconfirm --needed \
   fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool \
   wl-clip-persist \
   thunar sushi ffmpegthumbnailer gnome-calculator \
+  hyprsunset \
   gnome-keyring \
   brave-bin mpv \
   evince imv \

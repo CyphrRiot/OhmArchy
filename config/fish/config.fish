ee998cb06ea261aeed5b4e4b1ba38ea1de6dc0c0
Cypher Riot <193944077+HunterRose42@users.noreply.github.com>
2025-07-12 10:56:36 -0700

    🎨 Improve Git prompt aesthetics
    • Replace lightning bolt (⚡) with burnt orange bullet (●) for dirty state
    • Add upstream_equal character as empty to hide unnecessary = symbol
    • More intuitive and professional git status indicators

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -17,12 +17,14 @@ set -g __fish_git_prompt_showupstream yes
 set -g __fish_git_prompt_color_branch yellow
 set -g __fish_git_prompt_color_upstream_ahead green
 set -g __fish_git_prompt_color_upstream_behind red
-set -g __fish_git_prompt_char_dirtystate "⚡"
+set -g __fish_git_prompt_color_dirtystate 'ff8c00'
+set -g __fish_git_prompt_char_dirtystate "●"
 set -g __fish_git_prompt_char_stagedstate "→"
 set -g __fish_git_prompt_char_untrackedfiles "☡"
 set -g __fish_git_prompt_char_stashstate "↩"
 set -g __fish_git_prompt_char_upstream_ahead "+"
 set -g __fish_git_prompt_char_upstream_behind "-"
+set -g __fish_git_prompt_char_upstream_equal ""
 
 # Enhanced Prompt Function
 function fish_prompt
